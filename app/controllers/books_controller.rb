1b40215d1a273db0805ee19678aa8c708a75df0d
shinjun14 <shinjunsakano@gmail.com>
2021-09-09 08:45:44 +0000

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,14 +3,32 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
   end
 
   def show
-  end
-
-  def new
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :books
   get 'top' => 'books#top'
+  get 'books' => 'books#index'
+  post 'books' => 'books#create'
+  patch 'books/:id' => 'books#update', as: 'update_book'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
