e8912064142f29d8421ac36beff0a098c9fa03a4
shinjun14 <shinjunsakano@gmail.com>
2021-09-11 07:29:27 +0000

    [Finish]3-9

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,9 +8,14 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to books_path
+    @book = Book.new(book_params)
+    if  @book.save
+      　flash[:notice] = "Book was successfully created"
+      　redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -22,9 +27,21 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+      flash[:notice] = "Book was successfully updated"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to @book
+    flash[:notice] = "Book was successfully destroy"
+
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :books
+  root :to => 'books#top'
   get 'top' => 'books#top'
   get 'books' => 'books#index'
   post 'books' => 'books#create'
